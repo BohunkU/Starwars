fbd82849ca2810aeeaf8e53d9e7f3e498674dbdc
BohunkU <84118806+BohunkU@users.noreply.github.com>
2021-07-18 01:53:51 -0600

    Changing over to character display and sorting

--- scripts.js
+++ scripts.js
@@ -1,14 +1,46 @@
 import { people } from './people.js';
 import { starships } from './starships.js';
-console.log(starships[0])
+import { films } from './films.js';
+import { getLastNumber, removeChildren } from '../scripts/utils.js'
+
+// console.log(starships[0])
+
 //people data we want to use
-// birthday
-// homeworld
 // name
-//startshipts data we want to use
-// name
-// manufacture
-// model
+// gender
+
+//films data we want to use
+// title
+// episode_id
+
+const gallery = document.querySelector('.gallery')
+const femaleButton = document.querySelector('#femaleButton')
+const maleButton = document.querySelector('#maleButton')
+const robotButton = document.querySelector('#robotButton')
+
+const robotCharacters = people.filter(person => {
+    if (
+        person.gender === "hermaphrodite" || 
+        person.gender === "n/a" || 
+        person.gender === "none"
+    ) {
+        return person
+    }
+})
+
+maleButton.addEventListener('click', event => {
+    populateDOM(people.filter(person => person.gender === 'male'))
+})
+
+femaleButton.addEventListener('click', event => {
+    populateDOM(people.filter(person => person.gender === 'female'))
+})
+
+robotButton.addEventListener('click', event => {
+    populateDOM(robotCharacters)
+})
+
+
 let arrowFuncExample = (params) => {
     //do stuff
 }
